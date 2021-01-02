9315e0c6a2df0dfe5f39ea5ab7d78dccd127f481
hasanabir <vj.abir7@gmail.com>
2021-01-02 11:47:02 +0600

    Seeding

---
+++ seeders/20210102054342-demo-value.js
@@ -0,0 +1,23 @@
+'use strict'
+const { v4: uuidv4 } = require('uuid')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'values',
+      [
+        {
+          uuid: uuidv4(),
+          body: 'Customer Collaboration Over Contract Negotiation',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('values', null, {})
+  },
+}
---
+++ seeders/20210102054412-demo-value.js
@@ -0,0 +1,23 @@
+'use strict'
+const { v4: uuidv4 } = require('uuid')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'values',
+      [
+        {
+          uuid: uuidv4(),
+          body: 'Responding to Change Over Following a Plan',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('values', null, {})
+  },
+}
